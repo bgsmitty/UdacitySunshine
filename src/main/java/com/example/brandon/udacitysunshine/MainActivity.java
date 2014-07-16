dadd728ad2012ef718a821330146f949a09c7ef7
Brandon Smith <bgsmitty@gmail.com>
2014-07-16 07:41:12 -0600

    Adder HttpURLConnection code within the PlaceholderFragment Class

--- src/main/java/com/example/brandon/udacitysunshine/MainActivity.java
+++ src/main/java/com/example/brandon/udacitysunshine/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.brandon.udacitysunshine;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -63,8 +70,6 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
 
-            // Make sure rootView is initialize before the listview which will reference it
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             //Declaring String array to represent data
             String[] forecastArray = {
                     "Today - Sunny - 88/63",
@@ -76,23 +81,83 @@ public class MainActivity extends ActionBarActivity {
                     "Sun - Sunny - 80/68"
             };
 
-
             List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
             mForecastAdapter = new ArrayAdapter<String>(
-                    // The current context (this fragment's parent activity
-                    getActivity(),
-                    // ID of list item layout
-                    R.layout.list_item_forecast,
-                    // ID of the textview to populate
-                    R.id.list_item_forecast_textview,
-                    //Forecast data
-                    weekForecast
-            );
+                    getActivity(), // The current context (this fragment's parent activity
+                    R.layout.list_item_forecast, // ID of list item layout
+                    R.id.list_item_forecast_textview, // ID of the TextView to populate
+                    weekForecast); //Forecast data
 
+            // Make sure rootView is initialize before the ListView which will reference it
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // Get the reference to the ListView and attach this list adapter to it.
             ListView forecastListView = (ListView) rootView.findViewById(R.id.listview_forecast);
             forecastListView.setAdapter(mForecastAdapter);
 
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string
+            String forecastJsonStr = null;
+
+            try{
+                // Construct the URL for the OpenWeatherMap Query
+                // Possible parameters are available at OWM's forecast API page, at
+                // http://openweather.org/API#forecast
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    forecastJsonStr = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty. No point in parsing.
+                    forecastJsonStr = null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
+                forecastJsonStr = null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
             return rootView;
+
         }
+
     }
+
 }
